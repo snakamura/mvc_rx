ba805d4d185ab0a1d7dd9b419e154f5c82a6a4a8
Satoshi Nakamura <snakamura@asteria.com>
2021-05-07 15:22:09 +0900

    Add mvc v1

---
+++ 1/mvc/index.ts
@@ -0,0 +1,57 @@
+class Model {
+    get values(): number[] {
+        return [...this._values];
+    }
+
+    addValue(value: number): void {
+        this._values.push(value);
+        this.fireUpdated();
+    }
+
+    addListener(listener: ModelListener): void {
+        this._listeners.push(listener);
+    }
+
+    private fireUpdated(): void {
+        const event = new ModelEvent(this);
+        this._listeners.forEach(listener => listener.updated(event));
+    }
+
+    private _values: number[] = [];
+    private _listeners: ModelListener[] = [];
+}
+
+class ModelEvent {
+    constructor(readonly model: Model) {}
+}
+
+interface ModelListener {
+    updated(event: ModelEvent): void;
+}
+
+
+class Controller {
+    constructor(model: Model, document: HTMLDocument) {
+        const $value = document.getElementById('value') as HTMLInputElement;
+        const $add = document.getElementById('add') as HTMLInputElement;
+        const $view = document.getElementById('view') as HTMLInputElement;
+
+        $add.addEventListener('click', () => {
+            if ($value.value.length > 0) {
+                const v = Number($value.value);
+                if (!isNaN(v)) {
+                    model.addValue(v);
+                }
+            }
+            $value.value = '';
+        });
+
+        model.addListener({
+            updated(event: ModelEvent): void {
+                $view.innerText = event.model.values.join(', ');
+            }
+        });
+    }
+}
+
+new Controller(new Model(), document);
