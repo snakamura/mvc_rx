00687a748a9d50cce982c58b41715879b2b0ef65
Satoshi Nakamura <snakamura@asteria.com>
2021-05-07 20:51:32 +0900

    Add mvc v3
    Support loading.

---
+++ 3/mvc/index.ts
@@ -0,0 +1,80 @@
+class Model {
+    get values(): number[] {
+        return [...this._values];
+    }
+
+    addValue(value: number): void {
+        this._values.push(value);
+        this.fireUpdated();
+    }
+
+    save(): string {
+        return JSON.stringify(this._values);
+    }
+
+    load(values: string) {
+        this._values = JSON.parse(values);
+        this.fireUpdated();
+    }
+
+    addListener(listener: ModelListener): void {
+        this._listeners.push(listener);
+    }
+
+    private fireUpdated(): void {
+        const event = new ModelEvent(this);
+        this._listeners.forEach(listener => listener.updated(event));
+    }
+
+    private _values: number[] = [];
+    private _listeners: ModelListener[] = [];
+}
+
+class ModelEvent {
+    constructor(readonly model: Model) {}
+}
+
+interface ModelListener {
+    updated(event: ModelEvent): void;
+}
+
+
+class Controller {
+    constructor(model: Model, document: HTMLDocument) {
+        const $value = document.getElementById('value') as HTMLInputElement;
+        const $add = document.getElementById('add') as HTMLInputElement;
+        const $save = document.getElementById('save') as HTMLInputElement;
+        const $load = document.getElementById('load') as HTMLInputElement;
+        const $view = document.getElementById('view') as HTMLInputElement;
+
+        $add.addEventListener('click', () => {
+            if ($value.value.length > 0) {
+                const v = Number($value.value);
+                if (!isNaN(v)) {
+                    model.addValue(v);
+                }
+            }
+            $value.value = '';
+        });
+
+        $save.addEventListener('click', () => {
+            window.alert(model.save());
+        });
+
+        $load.addEventListener('click', () => {
+            const values = window.prompt('Load');
+            if (values != null) {
+                model.load(values);
+                $value.value = '';
+            }
+        });
+
+        model.addListener({
+            updated(event: ModelEvent): void {
+                $view.innerText = event.model.values.join(', ');
+            }
+        });
+    }
+}
+
+new Controller(new Model(), document);
